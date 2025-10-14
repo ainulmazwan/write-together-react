15f77e43908f6be1bbfd0b59e4670deeb0d44afa
Ainul Mardhiah <ainulmardhiah2muslim@gmail.com>
2025-10-14 20:36:43 +0800

    2nd push to server take two

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,9 @@
 export const API_URL = import.meta.env.DEV
   ? "http://localhost:5123/api/"
   : "https://b15-ainul.mak3r.dev/api/";
-export const DELETED_STORY_ID = "68e89428117cf3e55f9ae006";
-export const DELETED_CHAPTER_ID = "68ea4f67fcc2d405fd970bbc";
+export const DELETED_STORY_ID = import.meta.env.DEV
+  ? "68e89428117cf3e55f9ae006"
+  : "68ee3154169f82b32c2f4295";
+export const DELETED_CHAPTER_ID = import.meta.env.DEV
+  ? "68ea4f67fcc2d405fd970bbc"
+  : "68ee3154169f82b32c2f4299";
